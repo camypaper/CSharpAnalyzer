31e26b113da3e2f38c12effa6bcd343eef3f93bd
camypaper <camypaper@vivaldi.net>
2020-05-09 15:56:46 +0900

    Add Meta

---
+++ Records/Meta.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using Xunit;
+namespace CSharpAnalyzer.Records
+{
+    public class Meta
+    {
+        Dictionary<string, HashSet<string>> data = new Dictionary<string, HashSet<string>>();
+        public Meta() { }
+        public void Add(string target, string source)
+        {
+            if (!data.ContainsKey(target)) data[target] = new HashSet<string>();
+            data[target].Add(source);
+        }
+        public string PrintJson() => JsonSerializer.Serialize(data);
+    }
+    // TODO あとでテストプロジェクトに移す
+    public class MetaTest
+    {
+        [Fact(DisplayName = "Success to print Json")]
+        public void f()
+        {
+            var meta = new Meta();
+            for (int i = 0; i < 3; i++) meta.Add("key0", $"value{i % 2}");
+            for (int i = 0; i < 3; i++) meta.Add("key1", $"value{i}");
+            const string expected = "{\"key0\":[\"value0\",\"value1\"],\"key1\":[\"value0\",\"value1\",\"value2\"]}";
+            Assert.Equal(expected, meta.PrintJson());
+        }
+    }
+}
