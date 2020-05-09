8fc7947b3293f7a08d0ddef358e09c93e738e7bf
camypaper <camypaper@vivaldi.net>
2020-05-09 17:35:03 +0900

    Add Program

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using CSharpAnalyzer.Records;
+namespace CSharpAnalyzer
+{
+    class Program
+    {
+        static void Main()
+        {
+            var result = new Meta();
+            foreach (var x in Directory.EnumerateFiles(".", "*", SearchOption.AllDirectories)
+                                        .Where(x => x.EndsWith(".cs") || x.EndsWith(".csx")))
+            {
+                var parser = new Parser(x);
+                parser.Parse();
+                foreach (var y in parser.DependendClasses)
+                    result.Add(y, x);
+            }
+            Console.WriteLine(result.PrintJson());
+        }
+    }
+}
